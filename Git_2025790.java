b1ab0928fe278c7f31cc2905ec3dadc0cde2e0a2
pedroolg91-cmd <pedroolg91@gmail.com>
2025-12-07 20:03:14 +0100

    Add files via upload

---
+++ Git_2025790.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2025790;
+
+/**
+ *
+ * @author drped
+ */
+public class Git_2025790 {
+
+    public static void main(String[] args) {
+        System.out.println("Pedro Alberto Olguin Romero - Id_Student 2025790");
+    }
+}
+
+    
+    
+
